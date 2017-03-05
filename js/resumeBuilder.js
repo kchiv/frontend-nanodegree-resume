fb8e181f8e0ba4e389a55619c7ba38f254c67b25
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2017-03-05 10:46:29 -0800

    Add Js for name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%", "Kyle");
+var formattedRole = HTMLheaderRole.replace("%data%", "SEO Manager");
+
+
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
