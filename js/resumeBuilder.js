b2d68f27ada7857870d02c9ed8831a9f6490da0a
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2017-03-05 19:16:59 -0800

    Created bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,8 +2,26 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var formattedName = HTMLheaderName.replace("%data%", "Kyle Chivers");
-var formattedRole = HTMLheaderRole.replace("%data%", "SEO Manager");
+var bio = {
+	name: "Kyle Chivers",
+	role: "SEO Manager",
+	contactInfo: {
+		mobile: "(209)740-9122",
+		email: "chiverskyle@gmail.com",
+		twitter: "@chiverskyle",
+		github: "kchiv"
+	},
+	pictureURL: "images/fry.jpg",
+	welcomeMessage: "Hi, my name is Kyle.",
+	skills: ["SEO", "HTML", "CSS", "Javascript", "Python"]
+}
 
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%", bio.contactInfo.email).replace("%data%", bio.contactInfo.mobile);
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#topContacts").prepend(formattedContact);
+$("#header").prepend(formattedPic);
